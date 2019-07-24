302720875b59751fb78fd8c794bea68b1ee4f8d2
Atsui75 <34810709+Atsui75@users.noreply.github.com>
2019-07-24 18:28:24 +0200

    Actualización de Noise
    Devuelve datos de una forma más conveniente e incluye la posibilidad de iterar la función "iter" veces.

--- Noise.py
+++ Noise.py
@@ -1,9 +1,9 @@
 from Funciones import *
 
-def Noise(func, args, intervalo, data_size, caos = 0.1, espaciado = 0):
+def Noise(func, args, intervalo, data_size, caos = 0.1, espaciado = 0, iter = 1):
 	'''Este método convierte una función analítica en "datos" que podrían haber salido de un laboratorio
 		
-		Noise(func, args, interv, data_size, <incert, espaciado>)
+		Noise(func, args, interv, data_size, <incert, espaciado>, <iter>)
 		
 		func: función a utilizar (function);
 		args: argumentos de la función;
@@ -16,39 +16,39 @@ def Noise(func, args, intervalo, data_size, caos = 0.1, espaciado = 0):
 		espaciado: espaciado entre datos (número); 
 			espaciado == 0:		random (default)
 			espaciado != 0:		número introducido
+		iter: número de iteraciones (int);
+			iter = 1			(default)
 	'''
 	try:
 		assert len(intervalo) == 2
 		if data_size <= 0:
 			raise ValueError("data_size debe ser un número entero positivo")
+		if (type(iter) != int) or (iter <= 0):
+			raise TypeError("iter debe ser un número entero positivo")
 	except AssertionError:
 		print("Intervalo no tiene exactamente dos parámetros")
 	
-	if espaciado == 0:
-		data_pos = np.sort(np.random.rand(data_size-2)* data_size)	
-		np.insert(data_pos, 0, intervalo[0])
-		np.append(data_pos, intervalo[-1])							
+	randput = lambda y: y + (2*np.random.rand(len(y))-1) * caos		#Con caos modulamos la dispersión de los datos
+	
+	Datos = []
+	Output = []
+	
+	for i in range(iter):
+		if espaciado == 0:
+			data_pos = np.sort(np.random.rand(data_size-2)* data_size)	
+			np.insert(data_pos, 0, intervalo[0])
+			np.append(data_pos, intervalo[-1])							
+			
+		else:
+			data_pos = np.linspace(intervalo, data_size)		#Creamos las posiciones para los datos (eje horizontal)
 		
-	else:
-		data_pos = np.linspace(intervalo, data_size)		#Creamos las posiciones para los datos (eje horizontal)
-	
-	output = func(data_pos, args)
-	
-	Aleatorio = False
-	if len(output) == 2:
-		y = output[0]
-		args = output[1]
-		Aleatorio = True
-	else:
-		y = output
+		output = func(data_pos, args)		#datos, nombre, argumentos 
 		
-	data = y + (2*np.random.rand(len(y))-1) * caos 	#Con caos modulamos la dispersión de los datos
+		Datos.append(data_pos)
+		Output.append([list(np.vstack([data_pos,randput(output[0])]).T), output[1]]) #, output[2]
 	
-	if Aleatorio:
-		return np.vstack([data_pos,data]).T, np.vstack([data_pos, y]).T, args	#Matrices verticales con columnas x e y (Ruido // Sin Ruido)
-	else: 
-		return np.vstack([data_pos,data]).T, np.vstack([data_pos, y]).T		#Matrices verticales con columnas x e y (Ruido // Sin Ruido)
-	
+	return Output
+
 	
 	
 	
