7c1f15de54784f1974c0253a969783c9ce972fb6
Atsui75 <34810709+Atsui75@users.noreply.github.com>
2019-07-24 18:29:11 +0200

    Actualización de formato
    Actualización necesaria por compatibilidad con Noise

--- Funciones.py
+++ Funciones.py
@@ -14,18 +14,22 @@ def P(x,args = 2):
 		type(args) == int:		Grado del polinomio (parámetros aleatorios)(default)
 		type(args) != int :							(lista o array)
 	'''
+	nombre = 'PolinomioG'
+	
 	if type(args) == int:
+		if args < 0:
+			raise ValueError("args debe ser un entero positivo una lista o un array")
 		A = np.random.rand(args)	#El formato de esto hay que cambiarlo para cada tipo de función
 	elif (type(args) == list) or (type(args) == np.array):
 		A = np.array(args)
 	else:
-		raise TypeError("Args tiene que ser un entero, una lista o un array")
+		raise TypeError("args debe ser un entero positivo una lista o un array")
 	
 	P = np.zeros(len(x))				
 	for i in range(len(A)):			#Esta es la parte única de cada tipo de función, el resto puede ser Ctrl+C, Ctrl+V
 		P += A[i]*np.power(x,i)		#Aunque también hay que ajustar qué significa args int en cada una, preferiblemente aleatoriedad.
 		
 	if type(args) == int:
-		return P, A
+		return P, (nombre + str(args)), A
 	else: 
-		return P
+		return P, (nombre + str(len(args) -1)), A
