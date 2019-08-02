7d5dfe217006127633d4bddb788f0ef7e460d293
Atsui75 <34810709+Atsui75@users.noreply.github.com>
2019-08-02 23:39:21 +0200

    Simplificación
    Simplificación en el módulo y creación de funciones nuevas de cara al futuro.

--- Noise/Funciones.py
+++ Noise/Funciones.py
@@ -2,34 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 '''Listado de funciones disponibles:
-	- Polinomios
-
+	- Polinomios A + Bx + Cx^2 + ...
+	- Coseno Acos(Bx + C) + D
+	- Exponencial Ae^(Bx + C) + D
 
 '''
+
+def text(args):
+	if type(args) == int:
+		return str(args)
+	else:
+		return str(len(args) -1)
+		
+def Args(args):
+	'''Función de generación de argumentos
+	lista con elementos str da lugar a parámetro en esa posición aleatorio, por ej.: [0,'',1,3] => el parámetro B será aleatorio
+	'''
+
+	if type(args) == int:
+	
+		if args < 0:
+			raise ValueError("args debe ser un entero positivo una lista o un array")
+		return np.random.rand(args)
+		
+	elif (type(args) == list) or (type(args) == np.array):
+		return [i if type(i)!=str else np.random.rand() if i != '-' else 2*np.random.rand()-1 if  for i in args]
+		
+	else:
+		raise TypeError("args debe ser un entero positivo una lista o un array")
+
 def P(x,args = 2):
 	'''Función Polinomio
-	P(x,<args>) = P, A o  P (solo devolvemos A si es aleatorio)
+	P(x,<args>) = F, Nombre, A 
 	x : valores en x;
-	args : Términos A,B,C, ... del polinomio;
+	args : Términos A,B,C, ...;
 		type(args) == int:		Grado del polinomio (parámetros aleatorios)(default)
 		type(args) != int :							(lista o array)
 	'''
 	nombre = 'PolinomioG'
 	
-	if type(args) == int:
-		if args < 0:
-			raise ValueError("args debe ser un entero positivo una lista o un array")
-		A = np.random.rand(args)	#El formato de esto hay que cambiarlo para cada tipo de función
-	elif (type(args) == list) or (type(args) == np.array):
-		A = np.array(args)
-	else:
-		raise TypeError("args debe ser un entero positivo una lista o un array")
+	A = Args(args)
 	
-	P = np.zeros(len(x))				
-	for i in range(len(A)):			#Esta es la parte única de cada tipo de función, el resto puede ser Ctrl+C, Ctrl+V
-		P += A[i]*np.power(x,i)		#Aunque también hay que ajustar qué significa args int en cada una, preferiblemente aleatoriedad.
+	F = np.zeros(len(x))	
+	for i in range(len(A)):
+		F += A[i]*np.power(x,i)
 		
-	if type(args) == int:
-		return P, (nombre + str(args)), A
-	else: 
-		return P, (nombre + str(len(args) -1)), A
+	return F, (nombre + text(args)), A
+		
+def fexp(x,args = 4):
+	
+	nombre = 'Exponencial'
+		
+	A = Args(args)
+	
+	F = A[0]*np.e**(A[1]*x+A[2])+A[3]
+		
+	return F, nombre, A
+		
+def fcos(x,args = 4):
+	'''Función Coseno
+	'''
+	nombre = 'Coseno'
+	
+	A = Args(args)
+	
+	F = A[0]*np.cos(A[1]*x + A[2]) + A[3]
+		
+	return F, nombre, A
+	
